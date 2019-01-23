252694f269bbb87be8ee94f1fcd686f89ee17139
larynab <fsovreign@gmail.com>
2019-01-23 13:35:57 -0800

    this-beta

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,12 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global(window) binding means that the value of "this" is on a global scope. Meaning one huge container like  
+ window or console is operating as a global unit, everything applies to eachother.
+* 2. Implicit Binding is where "this" will be assigned to the object left of dot notation. Object is "this".
+* 3. New Binding is where within an Object the specific instance/attribute becomes "this" 
+ and will be returned by the object.
+* 4. Explicit is where "this" is defined through a call or apply method.
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +14,12 @@
 // Principle 1
 
 // code example for Window Binding
+function sayQuack(sound) {
+    console.log(this);
+    return sound; 
+};
+
+console.log(sayQuack("QUACK"));
 
 // Principle 2
 
